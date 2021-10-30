d0a7e676310510d8f4152a909b38b949446a7040
Franco <francoguardini10@gmail.com>
2021-10-30 15:12:47 +0200

    some changes here

--- sobre-nosotros.js
+++ sobre-nosotros.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const typed = new Typed('.typed', {
     strings: [
         'Dj Leo Ortega',
@@ -20,27 +19,4 @@ const typed = new Typed('.typed', {
     showCursor: true, // Mostrar cursor palpitanto
     cursorChar: '|', // Caracter para el cursor
     contentType: 'html', // 'html' o 'null' para texto sin formato
-=======
-const typed = new Typed('.typed', {
-    strings: [
-        'Dj Leo Ortega',
-        'Dj Pablo Ortega',
-        'Dj Lucho Ortega',
-        'Dj Pablo Scheff',
-        'Nosotros'
-    ],
-
-    //stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-    typeSpeed: 80, // Velocidad en mlisegundos para poner una letra,
-    startDelay: 70, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-    backSpeed: 70, // Velocidad en milisegundos para borrrar una letra,
-    smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
-    shuffle: false, // Alterar el orden en el que escribe las palabras.
-    backDelay: 70, // Tiempo de espera despues de que termina de escribir una palabra.
-    loop: true, // Repetir el array de strings
-    loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
-    showCursor: true, // Mostrar cursor palpitanto
-    cursorChar: '|', // Caracter para el cursor
-    contentType: 'html', // 'html' o 'null' para texto sin formato
->>>>>>> 7271feea685c53a9a36fff9b23e98bcd79850823
 })
